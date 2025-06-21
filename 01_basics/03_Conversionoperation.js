7fd10f20a1a7cae767c19fdb801a0dfce11142a7
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-21 17:11:12 +0000

    conversions

---
+++ 01_basics/03_Conversionoperation.js
@@ -0,0 +1,7 @@
+let age="23";
+console.log(typeof age);
+let age1= Number(age);
+console.log(typeof age1);
+
+
+// in this we have talk about the conversions to know about the type of data we can use typeof 
