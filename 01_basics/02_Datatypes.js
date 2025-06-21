a3402764c115b261846fd9aeccb9a5c5afdc9dd0
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-21 16:38:01 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,8 @@
+// in this we didnt learn something extra in js if we have to declare datatypes we need let only 
+let name="Arvind";
+let age=23;
+let boolean=true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof boolean);
