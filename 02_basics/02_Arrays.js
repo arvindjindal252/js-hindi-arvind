e51c44435a5070cfea52fa2d7d6de4610724a325
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-24 14:26:08 +0000

    Arrays basics 2

---
+++ 02_basics/02_Arrays.js
@@ -0,0 +1,10 @@
+// here we are learning about how we can add two arrays
+
+// Concat
+
+const hero = ["Shaktiman","hanuman"]
+const hero1 = ["spider","batman","superman"]
+
+const hero2= hero.concat(hero1);
+
+console.log(hero2);
