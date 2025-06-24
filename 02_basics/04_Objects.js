effa525f3ce2ab92e249c5c97f80d58c1edec0e1
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-24 19:48:40 +0000

    Object Destructing

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -6,3 +6,17 @@ tinderUSer.name="Arvind"
 tinderUSer.caste="General"
 tinderUSer.rollno=2121252
 console.log(tinderUSer);
+
+
+// object destructing 
+
+let structingobject={
+    coursename:'Maths',
+    coursefees:12345
+}
+const {coursefees} = structingobject  // destructing of the object 
+const {coursename} = structingobject 
+console.log(coursename)
+
+// here we talk about api 
+// the form of api is json (javascript object notation)
