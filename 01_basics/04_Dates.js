0b5cedac3b1523b44a368a1670318b778adae940
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-24 09:52:16 +0000

    Date and Time

---
+++ 01_basics/04_Dates.js
@@ -0,0 +1,9 @@
+let newDate = new Date();
+// when we are writing the new keyword it means we are declaring the object of that keyword
+
+console.log(newDate);
+console.log(newDate.toString());
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+console.log(typeof newDate);
+
