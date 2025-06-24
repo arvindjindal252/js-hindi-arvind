3f79e7bfef04d02e05a458c581896d70b30abe2b
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-24 13:53:45 +0000

    Arrays

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,17 @@
+const newArray = [0,1,2,3,4,5];
+// console.log(newArray[2])
+
+//Declaring array by creating its objects
+
+// const newArray1= new Array(1,2,3,4,5,6)
+// console.log(newArray1[4]);
+
+
+console.log(newArray);
+// console.log(newArray.slice(0,2));
+const newArray3=newArray.splice(0,2);
+console.log(newArray3);
+console.log(newArray)
+
+//  here we are learning slicing and splicing 
+//  Slicing give you a piece without changing the original one while splicing originally cut it 
