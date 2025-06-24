015a9ebe45205f15a7dd121181bce601701a515a
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-24 17:47:37 +0000

    Objects

---
+++ 02_basics/03_Objects.js
@@ -0,0 +1,9 @@
+const newUser={
+    name: "Arvind Jindal",
+    email: "arvindjindal320@gmail.com",
+    rollno: 2121252,
+    profession:"Engineer"
+}
+console.log(newUser);
+
+// This is the simple way to declare the object without the constructor 
