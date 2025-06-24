f61f41a298669c225c6227db660648a7e8a42e20
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-06-24 18:16:37 +0000

    Object part 2

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,8 @@
+// we can create a singleton object using constructor
+
+const tinderUSer = new Object()
+
+tinderUSer.name="Arvind"
+tinderUSer.caste="General"
+tinderUSer.rollno=2121252
+console.log(tinderUSer);
