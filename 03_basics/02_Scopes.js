d0ba82f56a9374457b5f82a63790874317ac4638
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-03 19:46:35 +0000

    Scopes

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,10 @@
+// in this we will talk about scope 
+
+if(true){
+let a=10;
+const b=20;
+var c=30;
+}
+
+console.log(c);
+// it will surely print it which will cause the issue this is not a block scoped 
