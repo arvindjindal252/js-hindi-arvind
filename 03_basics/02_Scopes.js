f72105d70335c0140054bd22eec3a67b6d92d989
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-07 17:15:51 +0000

    scopes depth

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -8,3 +8,14 @@ var c=30;
 
 console.log(c);
 // it will surely print it which will cause the issue this is not a block scoped 
+
+
+function add(){
+    const string="arvind";
+    if(true){
+        console.log(string);
+        const string2="abhinav";
+    }
+    // console.log(string2);// this will show you the error because of the scope 
+}
+add();
