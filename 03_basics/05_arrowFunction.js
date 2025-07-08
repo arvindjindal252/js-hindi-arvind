6c62cbbe1488911e61233e8136b39c9449cd2697
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-08 12:22:40 +0000

    arrow function

---
+++ 03_basics/05_arrowFunction.js
@@ -0,0 +1,25 @@
+// first way of writing function 
+
+function namingConventions(){
+    console.log("my name is arvind")
+}
+
+namingConventions();
+
+
+// Second Way of writting function
+
+const namingConventions1= function (){
+    console.log("my name is arvind")
+}
+
+namingConventions1();
+
+
+// third way of writting function and most important (arrow function)
+
+const namingConventions2= ()=>{
+    console.log("My name is Arvindjindal");
+}
+
+namingConventions2();
