3b91df2b3be1e3f6bc9a2aa841849b83b1075fa2
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-08 13:08:53 +0000

    iife

---
+++ 03_basics/06_iife.js
@@ -0,0 +1,7 @@
+//immeditely invoked function expressions 
+
+(function naming4(){
+    console.log("My name is Abhinav Jindal");
+})()
+
+// this immediately calls the function so that globally scope pollution se save ho ske 
