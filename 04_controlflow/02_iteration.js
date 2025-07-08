80b5ad5335997358927935ef93083b6b4cd8cf0c
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-08 19:53:06 +0000

    for loop

---
+++ 04_controlflow/02_iteration.js
@@ -0,0 +1,7 @@
+// in this we have to talk about the for loop 
+let fruits = ['apple','mango','banana'];
+for (let index = 0; index < fruits.length; index++) {
+    const element = fruits[index];
+    console.log(element);
+}
+
