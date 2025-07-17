c3eb8cc5563c871296f6d7c1fa8fcf2443341941
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-17 18:57:52 +0000

    forEach Loop

---
+++ 04_controlflow/04_iteration.js
@@ -0,0 +1,12 @@
+// best and strongest loop for array
+
+const values=["Bike","Scotty","Car","aeroplane"]
+
+values.forEach( (item)=>{
+console.log(item)
+})
+
+
+// normal foreach and apply arrowfunction
+
+// best strongest loop for arrays
