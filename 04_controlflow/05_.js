6645bb2c81d672f0feee7300bf77ba319c63c05e
Arvind Jindal <144128172+arvindjindal252@users.noreply.github.com>
2025-07-17 18:58:44 +0000

    Filter

---
+++ 04_controlflow/05_.js
@@ -0,0 +1,8 @@
+//here we will learn about filters
+
+const mynums=[1,2,3,4,5,6,7,8,9]
+
+const values= mynums.filter((num)=> num>4)
+// we have to write this in one line because this is more efiicient way 
+
+console.log(values);
